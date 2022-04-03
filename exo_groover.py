eb2d74b5a0d61f5e4849de5fcd80403845cd5cb7
othmane <othmaneeb@sipios.com>
2022-04-03 23:47:24 +0200

    feat: return True if there is a combination of n tracks that equals concert_premiere_length

--- exo_groover.py
+++ exo_groover.py
@@ -1,34 +1,22 @@
 
-tracks = {
-    1 : 10,
-    2 : 12,
-    3 : 5,
-    4 :7,
-    5 : 9
-}
-
-concert_premiere_length = 27
-print("---- Number of tracks : ",len(tracks)," ----")
-
-
-def retrieve_all_combinaisons_of_3_tracks(tracks):
+def retrieve_all_combinaisons_of_n_tracks(tracks,nb_tracks_for_premiere):
     tracklist_combinaisons=[]
     nb_tracks = len(tracks)
-    indices = list(tracks.keys())[0:3]
+    indices = list(tracks.keys())[0:nb_tracks_for_premiere]
     tracklist = dict(filter(lambda key: key[0] in indices, tracks.items()))
     tracklist_combinaisons.append(tracklist)
 
-    index = 2
+    index = nb_tracks_for_premiere-1
     while (index !=-1):
        indices[index] +=1
 
-       for j in range(index+1,3):
+       for j in range(index+1,nb_tracks_for_premiere):
          indices[j] = indices[j-1] +1
 
-       if indices[index]== (nb_tracks-1 + index):
+       if indices[index]== (nb_tracks-(nb_tracks_for_premiere-2) + index):
         index -=1
        else :
-        index = 2
+        index = nb_tracks_for_premiere-1
 
         tracklist = dict(filter(lambda key: key[0] in indices, tracks.items()))
         tracklist_combinaisons.append(tracklist)
@@ -36,16 +24,36 @@ def retrieve_all_combinaisons_of_3_tracks(tracks):
     return tracklist_combinaisons
 
 
-def is_combinaison_of_3_tracks_ok_for_concert_premiere(tracklist_combinaisons,concert_premiere_length):
+def is_combinaison_of_n_tracks_ok_for_concert_premiere(tracklist_combinaisons,concert_premiere_length):
     for tracklist in tracklist_combinaisons:
         tracklist_length= sum(tracklist.values())
         if tracklist_length == concert_premiere_length:
             return True
     return False
 
+#Initialize values
+tracks = {
+    1 : 10,
+    2 : 12,
+    3 : 5,
+    4 :7,
+    5 : 9
+}
+print("---- Number of tracks : ",len(tracks)," ----")
 
-tracklist_combinaisons = retrieve_all_combinaisons_of_3_tracks(tracks)
-print("---- Combinaisons possibles  : ",tracklist_combinaisons," ----")
-resultat = is_combinaison_of_3_tracks_ok_for_concert_premiere(tracklist_combinaisons, concert_premiere_length)
+nb_tracks_for_premiere = 4
+concert_premiere_length = 26
 
-print(resultat)
+
+
+def is_tracklist_ok_for_premier_concert(tracks, nb_tracks_for_premiere, concert_premiere_length):
+
+    """
+    Returns True if there is a tracklist of n elements that have a length equals to premiere_concert_length
+    """
+    tracklist_combinaisons = retrieve_all_combinaisons_of_n_tracks(tracks,nb_tracks_for_premiere)
+    print("---- Combinaisons possibles  : ",tracklist_combinaisons," ----")
+    resultat = is_combinaison_of_n_tracks_ok_for_concert_premiere(tracklist_combinaisons, concert_premiere_length)
+    return resultat
+
+print(is_tracklist_ok_for_premier_concert(tracks,nb_tracks_for_premiere,concert_premiere_length))
